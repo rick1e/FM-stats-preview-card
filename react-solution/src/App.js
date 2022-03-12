0a804a711f3150ea308cfd579e59b146036c63fd
rick1e <alrickab@gmail.com>
2022-03-12 15:39:54 -0500

    desktop build complete

--- react-solution/src/App.js
+++ react-solution/src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import team from './assets/image-header-desktop.jpg'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="main">
+        <div id="content">
+          <h2>Get <strong>insights</strong> that help your business grow.</h2>
+          <p>Discover the benefits of data analytics and make better decisions regarding revenue, customer experience, and overall efficiency.</p>
+          <div id="stats">
+            <div class="stat-item">
+              <h3>10k+</h3>
+              companies
+            </div>
+            <div class="stat-item">
+              <h3>314</h3>
+              templates
+            </div>
+            <div class="stat-item">
+              <h3>12M+</h3>
+              queries
+            </div>
+          </div>
+        </div>
+        <div id="highlight">
+          <img src={team} alt="TEAM" />
+        </div>
+      </div>
+
+      <div class="attribution">
+        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer">Frontend Mentor</a>.
+        Coded by <a href="https://github.com/rick1e">Alrick Brown</a>.
+      </div>
     </div>
   );
 }
